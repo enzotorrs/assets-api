a32e49963c2a731591a0c09363a4420fb0983835
enzotorrs <enzotorres969@gmail.com>
2024-04-13 12:51:30 -0300

    feat: add asset service passing in all unit tests

---
+++ src/assets/assets.service.ts
@@ -0,0 +1,37 @@
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { CreateAssetDto } from './dto/create-asset.dto';
+import { AssetRepository } from './repositories/asset.repository';
+import { Asset } from './entities/asset.entity';
+
+@Injectable()
+export class AssetsService {
+  constructor(
+    @Inject(AssetRepository) private assetRepository: AssetRepository,
+  ) {}
+
+  async create(assetToCreate: CreateAssetDto) {
+    const asset = new Asset();
+    if (assetToCreate.parentAssetId) {
+      const parentAsset = await this.assetRepository.findById(
+        assetToCreate.parentAssetId,
+      );
+      if (!parentAsset.folder) {
+        throw new HttpException(
+          'parent asset must be a folder',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+      asset.parentAsset = parentAsset;
+    }
+    this.assetRepository.merge(asset, assetToCreate);
+    return await this.assetRepository.save(asset);
+  }
+
+  async findAll() {
+    return await this.assetRepository.findAll();
+  }
+
+  async findOne(id: number) {
+    return await this.assetRepository.findById(id);
+  }
+}
