ec3d42c0cd66d7bb2c3446b70f199289352b3256
enzotorrs <enzotorres969@gmail.com>
2024-04-13 18:54:37 -0300

    feat: convert asset entity to a typeORM entity

--- src/assets/entities/asset.entity.ts
+++ src/assets/entities/asset.entity.ts
@@ -1,5 +1,25 @@
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
 export class Asset {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false })
   name: string;
+
+  @Column({ default: false })
   folder?: boolean;
+
+  @ManyToOne(() => Asset, (asset) => asset.parentAsset)
   parentAsset?: Asset;
+
+  @OneToMany(() => Asset, (asset) => asset.parentAsset)
+  childAssets?: Asset[];
 }
