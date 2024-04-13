779a9e9cffd95366a98dd637eb306ac44015bef9
enzotorrs <enzotorres969@gmail.com>
2024-04-13 20:26:31 -0300

    feat: create Patch endpoint for update name and parent asset of assets

--- src/assets/assets.controller.ts
+++ src/assets/assets.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Patch } from '@nestjs/common';
 import { AssetsService } from './assets.service';
 import { CreateAssetDto, UpdateAssetDto } from './asset.dto';
 
@@ -19,5 +19,10 @@ export class AssetsController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.assetsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() asset: UpdateAssetDto) {
+    return this.assetsService.update(+id, asset);
   }
 }
