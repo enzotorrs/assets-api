c895e4f446905ae5f9770b11c14f15a20ce32876
enzotorrs <enzotorres969@gmail.com>
2024-04-13 19:16:45 -0300

    feat: config db connetion in app module and import asset module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,21 @@
 import { Module } from '@nestjs/common';
+import { AssetsModule } from './assets/assets.module';
+import { Asset } from './assets/entities/asset.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    AssetsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'main',
+      entities: [Asset],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
