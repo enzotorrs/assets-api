31260cf43609bd842676476c6cac872b799a8e60
enzotorrs <enzotorres969@gmail.com>
2024-04-15 12:33:28 -0300

    feat: add user service and inject default repository

---
+++ src/users/users.service.ts
@@ -0,0 +1,44 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateUserDto, UpdateUserDto } from './users.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(user: CreateUserDto) {
+    return this.userRepository.save(user);
+  }
+
+  async findAll() {
+    return this.userRepository.find();
+  }
+
+  async findOne(id: number) {
+    return this.getUser(id);
+  }
+
+  async update(id: number, user: UpdateUserDto) {
+    const userToUpdate = await this.getUser(id);
+    userToUpdate.name = user.name;
+    return this.userRepository.save(userToUpdate);
+  }
+
+  async remove(id: number) {
+    const userToRemove = await this.getUser(id);
+    return this.userRepository.remove(userToRemove);
+  }
+
+  private async getUser(id: number) {
+    const user = await this.userRepository.findOneBy({ id: id });
+    if (!user) {
+      throw new HttpException('user not exists', HttpStatus.NOT_FOUND);
+    }
+    return user;
+  }
+}
